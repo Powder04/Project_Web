125e87009d602855c56f30bd7c241f45434a3137
Kiiv <jinzpower2580@gmail.com>
2025-06-01 00:02:40 +0700

    summary

--- account/register.php
+++ account/register.php
@@ -1,3 +1,21 @@
 <?php
-    
+    require_once("../mysqlConnect.php");
+    $mysqli->select_db("project");
+
+    function insert_customer($fullname, $username, $email, $pwd, $birth, $mysqli) {
+        $encrypt_pwd = password_hash($pwd, PASSWORD_DEFAULT); 
+        $stm = $mysqli->prepare("INSERT INTO customer(email, fullname, birthday, username, pwd) VALUES (?, ?, ?, ?, ?)");
+        $stm->bind_param("sssss", $email, $fullname, $birth, $username, $encrypt_pwd);
+        
+        if($stm->execute()){
+            header("Location: ./login.html");
+            exit();
+        } else {
+            echo "Lỗi đăng ký: " . $stm->error;
+        }
+
+        $stm->close();
+    }
+
+    insert_customer($_POST["fullname"], $_POST['username'], $_POST["email"], $_POST["pwd"], $_POST["birthday"], $mysqli);
 ?>
