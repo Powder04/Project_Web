6520e6584453812a009cdb65eecee07dc35dd15a
Kiiv <jinzpower2580@gmail.com>
2025-06-22 23:35:58 +0700

    summary

--- api/top_product.php
+++ api/top_product.php
@@ -4,7 +4,7 @@
     $stm = $mysqli->query("SELECT p.product_id, p.name, p.price, i.mime_type, TO_BASE64(i.image_data) 
                             AS image_data FROM product p
                             LEFT JOIN images i ON p.product_id = i.product_id
-                            ORDER BY p.sold_count LIMIT 12");
+                            ORDER BY p.sold_count DESC LIMIT 12");
     $data = [];
     while($row = $stm->fetch_assoc()) {
         $data[] = $row;
--- assets/javascript/push_top_product.js
+++ assets/javascript/push_top_product.js
@@ -5,12 +5,12 @@ async function topProduct() {
   var heroContent = document.getElementById("top_product");
   heroContent.innerHTML = data.map((product, index) =>
         `<div class="hero-content">
-                    <img src="data:${product.mime_type};base64,${product.image_data}"/>
-                        <p>${product.name}</p>
-                        <div class="price-container">
-                            <bdi class="price">${product.price}</bdi>
-                        </div>
-                </div>`).join("");
+            <img src="data:${product.mime_type};base64,${product.image_data}"/>
+                <p>${product.name}</p>
+                <div class="price-container">
+                  <bdi class="price">${product.price}</bdi>
+                </div>
+        </div>`).join("");
   var listPrice = document.getElementsByClassName("price");
   for (var i = 0; i < listPrice.length; i++) {
     var newPrice = parseInt(listPrice[i].innerHTML, 10).toLocaleString("vi-VN");
